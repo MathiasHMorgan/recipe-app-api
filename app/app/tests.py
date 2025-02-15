0cb6b1011a4fbbfe4dfc6041e17add1f5b83105b
Mathias Morgan <mathiasmorgan@192-168-1-11.tpgi.com.au>
2025-02-15 13:59:00 +0800

    added basic unit tests and db dependencies

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test calc Mod"""
+
+    def test_add_numbers(self):
+        """Test numbers add"""
+
+        res = calc.add(3,9)
+        self.assertEqual(res,12)
+    
+    def test_subtract_numbers(self):
+        """Test numbers subtract"""
+
+        res = calc.subtract(9,3)
+        self.assertEqual(res,6)
